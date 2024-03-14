c5b741fb8380166eee3b98cb396e80755d29802a
VITORIA AZEVEDO DA CRUZ <Vitoria Azevedo>
2024-03-14 17:25:22 -0300

    .

--- app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
@@ -3,24 +3,34 @@ package br.senai.sp.jandira.mytrips
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.PermIdentity
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -31,6 +41,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -131,14 +143,23 @@ fun Greeting() {
                     colors = OutlinedTextFieldDefaults.colors(unfocusedBorderColor = Color(0xFFCF06F0), focusedBorderColor = Color(0xFFCF06F0)),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(end = 14.dp)
+                        .padding(end = 14.dp),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = "password icon",
+                            tint = Color(0xFFCF06F0)
+                        )
+                    }
                 )
             }
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(end = 14.dp),
-                horizontalAlignment = Alignment.End
+                    .padding(end = 14.dp)
+                    .height(70.dp),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.SpaceBetween
             ){
                 Button(
                     onClick = {
@@ -163,10 +184,15 @@ fun Greeting() {
                 }
                 Row{
                     Text(
-                        text = "Don't have an account?"
+                        text = "Don't have an account?",
+                        color = Color(0xFFA09C9C),
+                        modifier = Modifier
+                            .padding(end = 4.dp)
                     )
                     Text(
-                        text = "Sign up"
+                        text = "Sign up",
+                        color = Color(0xFFCF06F0),
+                        fontWeight = FontWeight.Black
                     )
                 }
             }
@@ -188,5 +214,184 @@ fun Greeting() {
 fun GreetingPreview() {
     MyTripsTheme {
         Greeting()
+    }
+}
+
+@Composable
+fun CadastroTela(){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ){
+                Card(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(140.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFCF06F0)),
+                    shape = RoundedCornerShape(bottomStart = 14.dp)
+                ){}
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Sign up",
+                    color = Color(0xFFCF06F0),
+                    fontWeight = FontWeight.Black,
+                    fontSize = 32.sp
+                )
+                Text(
+                    text = "Create a new account",
+                    color = Color(0xFFA09C9C),
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Card(
+                    modifier = Modifier
+                        .size(80.dp),
+                    shape = CircleShape,
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFF6F6F6))
+                ){
+                    Icon(
+                        imageVector = Icons.Default.PermIdentity,
+                        contentDescription = "icon",
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        tint = Color(0xFFCF06F0)
+                    )
+                }
+                Card(
+                    modifier = Modifier
+                        .size(28.dp)
+                        .offset(x = 28.dp, y = 28.dp),
+                    shape = RectangleShape,
+                    colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+                ){
+                    Icon(
+                        imageVector = Icons.Default.AddAPhoto,
+                        contentDescription = "icon",
+                        tint = Color(0xFFCF06F0)
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = "Username")
+                    },
+                    modifier = Modifier
+                        .width(340.dp),
+                    shape = RoundedCornerShape(14.dp)
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = "Phone")
+                    },
+                    modifier = Modifier
+                        .width(340.dp),
+                    shape = RoundedCornerShape(14.dp)
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = "E-mail")
+                    },
+                    modifier = Modifier
+                        .width(340.dp),
+                    shape = RoundedCornerShape(14.dp)
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(text = "Password")
+                    },
+                    modifier = Modifier
+                        .width(340.dp),
+                    shape = RoundedCornerShape(14.dp)
+                )
+            }
+            Column {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Checkbox(
+                        checked = false,
+                        onCheckedChange = {},
+                        colors = CheckboxDefaults.colors(uncheckedColor = Color(0xFFCF06F0))
+                    )
+                    Text(
+                        text = "Over 18?"
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .width(340.dp)
+                        .height(48.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFCF06F0)),
+                    shape = RoundedCornerShape(14.dp)
+                ) {
+                   Text(
+                       text = "CREATE ACCOUNT",
+                       fontWeight = FontWeight.Black
+                   )
+                }
+            }
+            Row {
+                Text(
+                    text = "Already have an account?"
+                )
+                Text(
+                    text = "Sign in"
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ){
+                Card(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(140.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFCF06F0)),
+                    shape = RoundedCornerShape(topEnd = 14.dp)
+                ){}
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CadastroTelaPreview(){
+    MyTripsTheme {
+        CadastroTela()
     }
 }
